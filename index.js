be173f0dba6f800290cc5f0238a33b5a3c752fd5
Gareth <50303433+garethnoob@users.noreply.github.com>
2021-01-28 14:19:08 +0000

    add change innerhtml button

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const hideBulb = () => {
     el = document.getElementById('img');
     if (el.style.display === 'none') {
         el.style.display = 'block';
+        document.getElementById('btn').innerHTML = 'Hide Me!!';
     } else {
         el.style.display = 'none';
+        document.getElementById('btn').innerHTML = 'Show Me!!';
     }
 }
