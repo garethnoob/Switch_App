b314738e736e9cafe3b7ae8bf0e4e2bb23edd62c
Gareth <50303433+garethnoob@users.noreply.github.com>
2021-01-28 14:12:38 +0000

    add simple hide unhide

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Empty file awaiting content
+const hideBulb = () => {
+    el = document.getElementById('img');
+    if (el.style.display === 'none') {
+        el.style.display = 'block';
+    } else {
+        el.style.display = 'none';
+    }
+}
